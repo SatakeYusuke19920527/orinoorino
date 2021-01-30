81310a377d743a5d3cb8ed5aeae5a3630fe1b7e9
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-01-30 20:41:47 +0900

    utako

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ComponentA from './ComponentA';
-import ComponentB from './ComponentB';
-import ComponentC from './ComponentC';
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
 
 function App() {
   return (
---
+++ src/components/ComponentC.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const ComponentC = () => {
+    return (
+        <div>
+        <div>ComponentC</div>
+        <Link to="./">ComponentAへ移動</Link>
+        <Link to="componentb">ComponentBへ移動</Link>
+        </div>
+    );
+};
+
+export default ComponentC;
