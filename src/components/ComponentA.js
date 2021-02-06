687d62d74f841a2db5d8852f5fc13cc9f9473e29
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-02-06 20:40:30 +0900

    utako

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,12 +1,22 @@
 import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 
 const ComponentA = () => {
   const [ count,setCount ] = useState(0);
+  const [data,setData] = useState([])
 
   useEffect(() => {
     console.log('useEffect が呼び出されました。');
-  }, []);
+  }, [count]);
+  
+  axios.get('http://jsonplaceholder.typicode.com/todos')
+  .then(res => {
+    console.log(res,'res check')
+    setData(res.data)
+  })
+
+console.log(data)
 
     return (
         <div>
@@ -18,8 +28,8 @@ const ComponentA = () => {
             <p>{count}</p>
             <button onClick={() => setCount(count + 1)}>カウント</button>
         </div>
+
         </div>
-    );
-};
+    
 
 export default ComponentA;
